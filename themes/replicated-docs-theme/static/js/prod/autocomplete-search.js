70b8cdadc058729c891001c635c5fed51dcc0b9c
Sloane Smith <s10dimensional@gmail.com>
2018-03-06 16:08:21 -0800

    Added discourse to search, tweaked sidebar dropdown styles

--- themes/replicated-docs-theme/static/js/prod/autocomplete-search.js
+++ themes/replicated-docs-theme/static/js/prod/autocomplete-search.js
@@ -1,6 +1,7 @@
 const client = algoliasearch('5PHVQPX4AR', '92575152510e5cdaf7a0df17c446d879');
 const otherIndex = client.initIndex('help_center_other');
 const docsIndex = client.initIndex('help_center_docs');
+const discourseIndex = client.initIndex('discourse-posts');
 const guidesIndex = client.initIndex('help_center_guides');
 
 // Autocomplete search bar
@@ -12,7 +13,7 @@ autocomplete('#autocompletesearch-box', {
     }
 }, [{
         source: _.debounce(autocomplete.sources.hits(docsIndex, {
-            hitsPerPage: 2
+            hitsPerPage: 3
         }), 200),
         displayKey: 'title',
         name: 'docs',
@@ -24,7 +25,7 @@ autocomplete('#autocompletesearch-box', {
                 return '<h3 class="aa-suggestion-header">' +
                     suggestion._highlightResult.title.value + '</h3>' +
                     '<p class="aa-suggestion-description">' + (hasDescription ? suggestion._highlightResult.description.value : "") + '</p>' +
-                    '<span class="icon u-documentationIcon"></span>'
+                    `<span class="icon small u-${suggestion.icon ? suggestion.icon : "documentationIcon"}"></span>`
             },
             empty: "<div class='aa-empty'>No matching files</div>"
         }
@@ -66,10 +67,29 @@ autocomplete('#autocompletesearch-box', {
             },
             empty: "<div class='aa-empty'>No matching files</div>"
         }
-    }
+    },
+    {
+        source: _.debounce(autocomplete.sources.hits(discourseIndex, {
+            hitsPerPage: 2
+        }), 200),
+        displayKey: 'title',
+        name: 'discourse',
+        templates: {
+            header: '<h2 class="aa-header">Community</h2>',
+            suggestion: function(suggestion) {
+                const hasDescription = (suggestion._highlightResult.content).hasOwnProperty("value");
+                const title = suggestion._highlightResult.topic.title.value;
+                return '<h3 class="aa-suggestion-header">' + title + '</h3>' +
+                       '<p class="aa-suggestion-description">' + ( hasDescription ? suggestion._highlightResult.content.value : "") + '</p>'
+                       //`<span class="icon small u-${suggestion.icon ? suggestion.icon : "documentationIcon"}"></span>`
+            },
+            empty: "<div class='aa-empty'>No matching files</div>"
+        }
+    },
 ]).on('autocomplete:selected', function(e, suggestion, dataset) {
     // Fix for weird bug that places <em> tags in uri
-    window.location.href = suggestion._highlightResult.uri.value.replace(/<\/?[^>]+(>|$)/g, "");
+    window.location.href = dataset === "discourse" ? 
+        `https://help.replicated.com/community${suggestion.url}` : window.location.origin + "/" +suggestion._highlightResult.uri.value.replace(/<\/?[^>]+(>|$)/g, "");
 }).on('autocomplete:shown', function(e){
     $(this).keypress(function(e) {
         if(e.which === 13) {
@@ -86,7 +106,7 @@ autocomplete('#search-field', {
     },
 }, [{
         source: _.debounce(autocomplete.sources.hits(docsIndex, {
-            hitsPerPage: 2
+            hitsPerPage: 3
         }), 200),
         displayKey: 'title',
         name: 'docs',
@@ -98,7 +118,7 @@ autocomplete('#search-field', {
                 return '<h3 class="aa-suggestion-header">' +
                     suggestion._highlightResult.title.value + '</h3>' +
                     '<p class="aa-suggestion-description">' + (hasDescription ? suggestion._highlightResult.description.value : "") + '</p>' +
-                    '<span class="icon u-documentationIcon"></span>'
+                    `<span class="icon small u-${suggestion.icon ? suggestion.icon : "documentationIcon"}"></span>`
             },
             empty: "<div class='aa-empty'>No results</div>"
         },
@@ -140,10 +160,29 @@ autocomplete('#search-field', {
             },
             empty: "<div class='aa-empty'>No results</div>"
         }
-    }
+    },
+    {
+        source: _.debounce(autocomplete.sources.hits(discourseIndex, {
+            hitsPerPage: 2
+        }), 200),
+        displayKey: 'title',
+        name: 'discourse',
+        templates: {
+            header: '<h2 class="aa-header">Community</h2>',
+            suggestion: function(suggestion) {
+                const hasDescription = (suggestion._highlightResult.content).hasOwnProperty("value");
+                const title = suggestion._highlightResult.topic.title.value;
+                return '<h3 class="aa-suggestion-header">' + title + '</h3>' +
+                       '<p class="aa-suggestion-description">' + ( hasDescription ? suggestion._highlightResult.content.value : "") + '</p>'
+                       //`<span class="icon small u-${suggestion.icon ? suggestion.icon : "documentationIcon"}"></span>`
+            },
+            empty: "<div class='aa-empty'>No matching files</div>"
+        }
+    },
 ]).on('autocomplete:selected', function(e, suggestion, dataset) {
     // Fix for weird bug that places <em> tags in uri
-    window.location.href = window.location.origin + '/' + suggestion._highlightResult.uri.value.replace(/<\/?[^>]+(>|$)/g, "");
+    window.location.href = dataset === "discourse" ? 
+        `https://help.replicated.com/community${suggestion.url}` : window.location.origin + "/" +suggestion._highlightResult.uri.value.replace(/<\/?[^>]+(>|$)/g, "");
 }).on('autocomplete:shown', function(e){
     $(this).keypress(function(e) {
         if(e.which === 13) {
--- themes/replicated-docs-theme/static/js/staging/autocomplete-search-kubernetes.js
+++ themes/replicated-docs-theme/static/js/staging/autocomplete-search-kubernetes.js
@@ -2,81 +2,7 @@ const client = algoliasearch('5PHVQPX4AR', '92575152510e5cdaf7a0df17c446d879');
 const otherIndex = client.initIndex('help_center_staging_other');
 const docsIndex = client.initIndex('help_center_staging_docs_kubernetes');
 const guidesIndex = client.initIndex('help_center_staging_guides');
-
-// Autocomplete search bar
-autocomplete('#autocompletesearch-box', {
-    hint: false,
-    templates: {
-        dropdownMenu: "<div class='aa-dataset-other'></div>" +
-            "<div class='aa-dataset-docs'></div>"
-    }
-}, [{
-        source: _.debounce(autocomplete.sources.hits(docsIndex, {
-            hitsPerPage: 3
-        }), 200),
-        displayKey: 'title',
-        name: 'docs',
-        templates: {
-            header: '<h2 class="aa-header">Docs</h2>',
-            suggestion: function(suggestion) {
-                const hasDescription = (suggestion._highlightResult).hasOwnProperty("description");
-
-                return '<h3 class="aa-suggestion-header">' +
-                    suggestion._highlightResult.title.value + '</h3>' +
-                    '<p class="aa-suggestion-description">' + (hasDescription ? suggestion._highlightResult.description.value : "") + '</p>' +
-                    '<span class="icon u-documentationIcon"></span>'
-            },
-            empty: "<div class='aa-empty'>No matching files</div>"
-        }
-    },
-    {
-        source: _.debounce(autocomplete.sources.hits(guidesIndex, {
-            hitsPerPage: 2
-        }), 200),
-        displayKey: 'title',
-        name: 'guides',
-        templates: {
-            header: '<h2 class="aa-header">Guides</h2>',
-            suggestion: function(suggestion) {
-                const hasDescription = (suggestion._highlightResult).hasOwnProperty("description");
-
-                return '<h3 class="aa-suggestion-header">' +
-                    suggestion._highlightResult.title.value + '</h3>' +
-                    '<p class="aa-suggestion-description">' + (hasDescription ? suggestion._highlightResult.description.value : "") + '</p>' +
-                    '<span class="icon u-guidesIcon"></span>'
-            },
-            empty: "<div class='aa-empty'>No matching files</div>"
-        }
-    },
-    {
-        source: _.debounce(autocomplete.sources.hits(otherIndex, {
-            hitsPerPage: 2
-        }), 200),
-        displayKey: 'title',
-        name: 'replicated-other',
-        templates: {
-            header: '<h2 class="aa-header">Other</h2>',
-            suggestion: function(suggestion) {
-                const hasDescription = (suggestion._highlightResult).hasOwnProperty("description");
-                const hasTitle = (suggestion._highlightResult).hasOwnProperty("title");
-
-                return '<h3 class="aa-suggestion-header">' +
-                    (hasTitle ? suggestion._highlightResult.title.value : "") + '</h3>' +
-                    '<p class="aa-suggestion-description">' + (hasDescription ? suggestion._highlightResult.description.value : "") + '</p>'
-            },
-            empty: "<div class='aa-empty'>No matching files</div>"
-        }
-    }
-]).on('autocomplete:selected', function(e, suggestion, dataset) {
-    // Fix for weird bug that places <em> tags in uri
-    window.location.href = suggestion._highlightResult.uri.value.replace(/<\/?[^>]+(>|$)/g, "");
-}).on('autocomplete:shown', function(e){
-    $(this).keypress(function(e) {
-        if(e.which === 13) {
-           window.location.href = window.location.origin + '/search?q=' + this.value;  
-        }
-    });
-});
+const discourseIndex = client.initIndex('discourse-posts');
 
 autocomplete('#search-field', {
     hint: false,
@@ -139,10 +65,29 @@ autocomplete('#search-field', {
             },
             empty: "<div class='aa-empty'>No results</div>"
         }
-    }
+    },
+    {
+        source: _.debounce(autocomplete.sources.hits(discourseIndex, {
+            hitsPerPage: 2
+        }), 200),
+        displayKey: 'title',
+        name: 'discourse',
+        templates: {
+            header: '<h2 class="aa-header">Community</h2>',
+            suggestion: function(suggestion) {
+                const hasDescription = (suggestion._highlightResult.content).hasOwnProperty("value");
+                const title = suggestion._highlightResult.topic.title.value;
+                return '<h3 class="aa-suggestion-header">' + title + '</h3>' +
+                       '<p class="aa-suggestion-description">' + ( hasDescription ? suggestion._highlightResult.content.value : "") + '</p>'
+                       //`<span class="icon small u-${suggestion.icon ? suggestion.icon : "documentationIcon"}"></span>`
+            },
+            empty: "<div class='aa-empty'>No matching files</div>"
+        }
+    },
 ]).on('autocomplete:selected', function(e, suggestion, dataset) {
     // Fix for weird bug that places <em> tags in uri
-    window.location.href = window.location.origin + '/' + suggestion._highlightResult.uri.value.replace(/<\/?[^>]+(>|$)/g, "");
+    window.location.href = dataset === "discourse" ? 
+        `https://help.replicated.com/community${suggestion.url}` : window.location.origin + "/" +suggestion._highlightResult.uri.value.replace(/<\/?[^>]+(>|$)/g, "");
 }).on('autocomplete:shown', function(e){
     $(this).keypress(function(e) {
         if(e.which === 13) {
